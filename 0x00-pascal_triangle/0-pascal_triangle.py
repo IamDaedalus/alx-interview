f8f0088e3072762ec7063d26bedf5b90e5f09fe5
Daedalus <manny.is.daedalus@pm.me>
2024-03-10 18:55:48 +0000

    solved pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+def pascal_triangle(n):
+    """ this creates a pascal triangle of n rows """
+    triangle = []
+
+    # i is the outer iterator for each list of numbers
+    # j is the inner iterator for each number in the list
+
+    for i in range(n):
+        # first we create a list of 1s that we can then manipulate
+        # at when calculating the inner numbers
+        row = [1] * (i + 1)
+        # once we reach the 3rd row, we can start calculating the inner numbers
+        if i >= 2:
+            # specify a range that cuts across all numbers except the first and
+            # last numbers in the list
+            for j in range(1, i):
+                # calculate the inner numbers by adding the numbers in the
+                # previous row that are directly above them
+                # this works because triangle is a list of lists
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        # append the row to the triangle list
+        triangle.append(row)
+
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+This repo contains an implementation of the Pascal Triangle in Python
